efd7133368c6ce34be31ec0888854101bf55e8f4
MARAMBE00 <pasindurmarambe00@gmail.com>
2023-04-21 21:37:15 +0530

    Add CRUD operations

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('todo.ejs');
+app.get('/', async(req, res) => {
+    try {
+        const tasksresults = await TodoTask.find({},);
+        res.render("todo.ejs", { todoTasks: tasksresults });
+    } catch (err) {
+        res.send(500, err);
+        res.redirect("/");
+    }
 });
 
 app.post('/', async(req, res) => {
@@ -34,3 +40,38 @@ app.post('/', async(req, res) => {
         res.redirect("/");
     }
 });
+
+app.route("/remove/:id").get(async(req, res) => {
+    const id = req.params.id;
+    await TodoTask.findByIdAndRemove(id);
+    try {
+        res.redirect("/");
+    }
+    catch (err) {
+        res.send(500, err);
+        res.redirect("/");
+    }
+})
+
+app
+    .route("/edit/:id")
+    .get(async(req, res) => {
+        const id = req.params.id;
+        try {
+        const taskedits = await TodoTask.find({},);
+        res.render("todoEdit.ejs", { todoTasks: taskedits, idTask: id });
+        } catch (err) {
+        res.send(500, err); 
+        res.redirect("/");
+        }
+    })
+    .post(async(req, res) => {
+        const id = req.params.id;
+        await TodoTask.findByIdAndUpdate(id, { content: req.body.content });
+        try {
+        res.redirect("/");
+        } catch (err) {
+        res.send(500, err);
+        res.redirect("/");
+        }
+    });
